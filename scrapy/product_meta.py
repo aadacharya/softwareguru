20dc53f98af97248605c23be13501a64dd3b51a4
Aaditya <aadacharya@gmail.com>
2024-05-18 19:25:59 +0545

    scrapy with data

--- scrapy/product_meta.py
+++ scrapy/product_meta.py
@@ -1,50 +1,52 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-index = 0
+import re
+import json
+product_meta_map = None
+with open("data/product_meta_data.json" , "r") as f :
+    product_meta_map = json.load(f)
+# product_meta_map
+
 driver = webdriver.Chrome()
 url = "https://www.g2.com/search?utf8=%E2%9C%93&query=a&filters%5Bstar_rating%5D%5B%5D=5&order=popular"
 driver.get(url)
 import time
 for _ in range(5000):
-    map = {}
     element = driver.find_element(By.CSS_SELECTOR, "li.pagination__component.pagination__page-number.pagination__page-number--current")
     page_number = element.text
-    map[page_number] = {}
-    elements = driver.find_elements(By.CLASS_NAME, "paper.mb-1")
-    len(elements)
-    if len(elements) == 0 :
+    product_meta_map[page_number] = {}
+    product_elements = driver.find_elements(By.CSS_SELECTOR, 'div[class="paper mb-1"]')
+    if len(product_elements) == 0 :
         driver.refresh()
         continue
-    for element in elements:
+    for index,product_element in enumerate(product_elements):
         try:
-            product_name_div = element.find_element(By.CSS_SELECTOR, 'div[itemprop="name"]')
+            product_review_parent = product_element.find_element(By.CSS_SELECTOR, "a.product-listing__img.js-log-click")
+            product_review_link = product_review_parent.get_attribute("href")
+            product_name_div = product_element.find_element(By.CSS_SELECTOR, 'div[itemprop="name"]')
             product_name = product_name_div.text.strip()
-            title_to_find = f"Read {product_name} Reviews"
-            print(title_to_find)
-            parent_element = element.find_element(By.XPATH, "//a[@title='" + title_to_find + "']")
-            print(parent_element)
-            reviews_link = parent_element.get_attribute("href")
-            reviews_span = parent_element.find_element(By.CSS_SELECTOR, "span.pl-4th")
-            reviews_text = reviews_span.text
-            print("Reviews:", reviews_text)
-            product_paragraph_span = element.find_element(By.CLASS_NAME, 'product-listing__paragraph')
+            star_element = product_element.find_element(By.CSS_SELECTOR, "div.stars")
+            class_attribute = star_element.get_attribute("class")
+            rating = int(re.search(r'stars-(\d+)', class_attribute).group(1))/2
+            product_paragraph_span = product_element.find_element(By.CSS_SELECTOR, "span.product-listing__paragraph")
             product_paragraph = product_paragraph_span.text.strip()
-            product_image = element.find_element(By.CSS_SELECTOR, 'img[itemprop="image"]')
-            image_src_1 = product_image.get_attribute("data-deferred-image-src")
-            image_src = product_image.get_attribute("src")
-            parent_div = driver.find_element(By.CSS_SELECTOR, "div.product-listing__search-footer > div.cell.xlarge-8")
+            if product_paragraph_span.get_attribute("data-truncate-revealer-overflow-text"):  
+                product_paragraph = product_paragraph[:-12] + product_paragraph_span.get_attribute("data-truncate-revealer-overflow-text")
+            product_image = product_element.find_element(By.CSS_SELECTOR, 'img[itemprop="image"]')  
+            image_source = product_image.get_attribute("src") if product_image.get_attribute("src").startswith("http") else product_image.get_attribute("data-deferred-image-src")
+            categories_parent = product_element.find_element(By.CSS_SELECTOR, "div.product-listing__search-footer > div.cell.xlarge-8")
             categories = []
-            category_links = parent_div.find_elements(By.CSS_SELECTOR, "a.link.js-log-click")
+            category_links = categories_parent.find_elements(By.CSS_SELECTOR, "a.link.js-log-click")
             for link in category_links:
                 categories.append(link.text)
-            if index not in map[page_number]: map[page_number][index] = [ product_name,product_paragraph,[image_src,image_src_1],categories,[reviews_text,reviews_link]]
-            index+=1
+            print(page_number,index)
+            product_meta_map[page_number][index] = {"PRODUCT_REVIEW_LINK":product_review_link,"PRODUCT_NAME":product_name,"RATING":rating,"ABOUT":product_paragraph,"LOGO":image_source,"CATEGORIES":categories}
         except :
-            print("error")
+            print(f"error in page {page_number} and index {index}" )
     import json
-    with open(f"extracted_meta{page_number}.json", "w") as json_file:
-        json.dump(map, json_file, indent=4)
+    with open(f"data/product_meta_data{page_number}.json", "w") as json_file:
+        json.dump(product_meta_map, json_file, indent=4)
     next_link = driver.find_element(By.LINK_TEXT, "Next ›")
     next_link.click()
     time.sleep(2)
