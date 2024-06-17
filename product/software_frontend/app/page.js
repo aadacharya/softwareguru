7f5ab853364fbf0ed6d6c1f9ad6eec4e21b569ba
aadacharya <aadacharya@gmail.com>
2024-06-17 12:31:55 +0545

    frontend code

--- product/software_frontend/app/page.js
+++ product/software_frontend/app/page.js
@@ -3,13 +3,13 @@ import Header from "./_components/header/header";
 import Footer from "./_components/footer/footer";
 import styles from './home.module.css'
 import Link from "next/link";
-import Product from "./_components/product_abstract/product";
+import Product_Card from "./_components/product_card/product_card";
 
 export default function Home() {
   return (
     <div className={styles.homemain}>
       <Header/>
-      <Product/>
+      <Product_Card/>
       <Footer/>
     </div>
   );
