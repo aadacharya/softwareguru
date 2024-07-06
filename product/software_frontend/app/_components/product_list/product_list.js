4a210042367b4c9e67a15536223265ca51e91473
Ujjal Khadka <khadkauzal@gmail.com>
2024-07-06 18:33:57 +0545

    made the previous button disabled on page 1

--- product/software_frontend/app/_components/product_list/product_list.js
+++ product/software_frontend/app/_components/product_list/product_list.js
@@ -5,13 +5,14 @@ import styles from './product_list.module.css';
 import ProductCardBig from '../product_card_big/product_card_big';
 
 const ProductList = async ({ categories_list, page_number }) => {
-  page_number = parseInt(page_number);
+  page_number = parseInt(page_number) || 1;
   const previousPage = Math.max(1, page_number - 1);
   const nextPage = page_number + 1;
   let categories_param =
     categories_list.length === 1 && categories_list[0] === 'home'
       ? ''
       : categories_list.join(',');
+
   console.log(
     '##### ------->',
     categories_list,
@@ -19,16 +20,20 @@ const ProductList = async ({ categories_list, page_number }) => {
     previousPage,
     nextPage
   );
+
   if (categories_list.length === 0) {
     categories_list = ['home'];
   }
+
   let search_url = `http://127.0.0.1:8000/softwareguru/search?categories_list=${categories_param}&offset=${page_number}`;
   const res = await fetch(search_url, { cache: 'no-store' });
   if (!res.ok) {
     console.error('Failed to fetch data:', res.statusText);
     return <div>Error: Failed to fetch data</div>;
   }
+
   const products_data = await res.json();
+
   return (
     <div className={styles.productListMain}>
       {products_data.map((product_data) => (
@@ -39,9 +44,13 @@ const ProductList = async ({ categories_list, page_number }) => {
       ))}
       <div className={styles.pageChange}>
         <div className={styles.previous}>
-          <Link href={`/${categories_list}/${previousPage}`}>
-            &lt; Previous
-          </Link>
+          {page_number > 1 ? (
+            <Link href={`/${categories_list}/${previousPage}`}>
+              &lt; Previous
+            </Link>
+          ) : (
+            <p className={styles.disabled}>&lt; Previous</p>
+          )}
         </div>
         <div className={styles.next}>
           <Link href={`/${categories_list}/${nextPage}`}>Next &gt;</Link>
@@ -50,4 +59,5 @@ const ProductList = async ({ categories_list, page_number }) => {
     </div>
   );
 };
+
 export default ProductList;
