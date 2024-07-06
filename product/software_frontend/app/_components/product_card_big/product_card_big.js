de2f5075d6120760b547d328cd8e4f71d70984b9
Ujjal Khadka <khadkauzal@gmail.com>
2024-07-06 19:31:39 +0545

    created category and categorylist components

--- product/software_frontend/app/_components/product_card_big/product_card_big.js
+++ product/software_frontend/app/_components/product_card_big/product_card_big.js
@@ -1,62 +1,65 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import styles from "./product_card_big.module.css";
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './product_card_big.module.css';
+import Category from '../category/category';
+import CategoryList from '../categoryList/categoryList';
 
-const ProductCardBig = ({product_data}) => {
-  const  product_name= product_data["product_name"];
-  const  product_unique_id  = product_data["product_unique_id"];
-  const product_page_url = `/product/${encodeURIComponent(product_name)}/${encodeURIComponent(product_unique_id)}`;
+const ProductCardBig = ({ product_data }) => {
+  const product_name = product_data['product_name'];
+  const product_unique_id = product_data['product_unique_id'];
+  const product_page_url = `/product/${encodeURIComponent(
+    product_name
+  )}/${encodeURIComponent(product_unique_id)}`;
 
   return (
-      <div className={styles.productCardMain}>
-        <div className={styles["productHead"]}>
-          <div className={styles["productName"]}>
-            <Link href={product_page_url}>
-              <div className={styles["name"]}>{product_data["product_name"]}</div>
-            </Link>
-            <div className={styles["productLink"]}>
-              <Link href={product_data["product_url"]}>
-                <Image
-                  src="/external_link.svg"
-                  width={24}
-                  height={24}
-                  alt="Website"
-                />
-              </Link>
-            </div>
-          </div>
+    <div className={styles.productCardMain}>
+      <div className={styles['productHead']}>
+        <div className={styles['productName']}>
           <Link href={product_page_url}>
-            <div className={styles["productRating"]}>
-              <Image width={24} height={24} alt="Star" src="/star.svg" />
-              <h1>{product_data["product_rating"]}/10</h1>
-            </div>
+            <div className={styles['name']}>{product_data['product_name']}</div>
           </Link>
+          <div className={styles['productLink']}>
+            <Link href={product_data['product_url']}>
+              <Image
+                src="/external_link.svg"
+                width={24}
+                height={24}
+                alt="Website"
+              />
+            </Link>
+          </div>
         </div>
-        
-          <div className={styles["productImage"]}>
-          <Link href={product_page_url}>
-          <Image width={3000} height={1000} alt='Logo' src={product_data["images"][0]["image"]}/>
+        <Link href={product_page_url}>
+          <div className={styles['productRating']}>
+            <Image width={24} height={24} alt="Star" src="/star.svg" />
+            <h1>{product_data['product_rating']}/10</h1>
+          </div>
         </Link>
-          </div>
-        <div className={styles["productAbout"]}>
-        {product_data["product_summary"]}
-        </div>
-        <div className={styles["productTail"]}>
-          <div className={styles["productCategoires"]}>
-          {product_data["product_categories"].map((items) => (
-            <div key={items} className={styles["productCategory"]}> 
-              <Link href={`/${items}/1`}>
-                {items}
-              </Link>
-                </div>
-                  ))}
-          </div>
-          <div className={styles["productFeatured"]}>
-            { product_data["product_featured"] && (<Image src="/featured.svg" width={70} height={70} alt="Featured" />)}
-          </div>
+      </div>
+
+      <div className={styles['productImage']}>
+        <Link href={product_page_url}>
+          <Image
+            width={3000}
+            height={1000}
+            alt="Logo"
+            src={product_data['images'][0]['image']}
+          />
+        </Link>
+      </div>
+      <div className={styles['productAbout']}>
+        {product_data['product_summary']}
+      </div>
+      <div className={styles['productTail']}>
+        <CategoryList categoryList={product_data['product_categories']} />
+        <div className={styles['productFeatured']}>
+          {product_data['product_featured'] && (
+            <Image src="/featured.svg" width={70} height={70} alt="Featured" />
+          )}
         </div>
       </div>
+    </div>
     // </Link>
   );
 };
